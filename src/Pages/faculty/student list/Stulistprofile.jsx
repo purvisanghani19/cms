910f97196ee1d963e6cf12c9e1faab9faf40c98e
purvisanghani74 <purvisanghani1@gmail.com>
2023-03-02 13:26:30 +0530

    studentlist

--- src/Pages/faculty/student list/Stulistprofile.jsx
+++ src/Pages/faculty/student list/Stulistprofile.jsx
@@ -1,159 +1,125 @@
 import React from 'react';
 import WithLayout from '../../../components/common/comfaculty/Sidebar/SideBar';
+import "./stulistprofile.css";
+
+
+
+
+
+
 const Stulistprofile = () => {
     return (
         <>
-
-            <div className="container bootstrap snippets bootdey">
-                <div className="row">
-                    <div className="profile-nav col-md-3">
-                        <div className="panel">
-                            <div className="user-heading round">
-                                <a href="#">
-                                    <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="" />
-                                </a>
-                                <h1>Camila Smith</h1>
-                                <p>deydey@theEmail.com</p>
-                            </div>
-
-                            <ul className="nav nav-pills nav-stacked">
-                                <li className="active"><a href="#"> <i className="fa fa-user"></i> Profile</a></li>
-                                <li><a href="#"> <i className="fa fa-calendar"></i> Recent Activity <span className="label label-warning pull-right r-activity">9</span></a></li>
-                                <li><a href="#"> <i className="fa fa-edit"></i> Edit profile</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="profile-info col-md-9">
-                        <div className="panel">
-                            <form>
-                                <textarea placeholder="Whats in your mind today?" rows="2" className="form-control input-lg p-text-area"></textarea>
-                            </form>
-                            <footer className="panel-footer">
-                                <button className="btn btn-warning pull-right">Post</button>
-                                <ul className="nav nav-pills">
-                                    <li>
-                                        <a href="#"><i className="fa fa-map-marker"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i className="fa fa-camera"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i className=" fa fa-film"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i className="fa fa-microphone"></i></a>
-                                    </li>
-                                </ul>
-                            </footer>
-                        </div>
-                        <div className="panel">
-                            <div className="bio-graph-heading">
-                                Aliquam ac magna metus. Nam sed arcu non tellus fringilla fringilla ut vel ispum. Aliquam ac magna metus.
-                            </div>
-                            <div className="panel-body bio-graph-info">
-                                <h1>Bio Graph</h1>
-                                <div className="row">
-                                    <div className="bio-row">
-                                        <p><span>First Name </span>: Camila</p>
-                                    </div>
-                                    <div className="bio-row">
-                                        <p><span>Last Name </span>: Smith</p>
-                                    </div>
-                                    <div className="bio-row">
-                                        <p><span>Country </span>: Australia</p>
-                                    </div>
-                                    <div className="bio-row">
-                                        <p><span>Birthday</span>: 13 July 1983</p>
-                                    </div>
-                                    <div className="bio-row">
-                                        <p><span>Occupation </span>: UI Designer</p>
-                                    </div>
-                                    <div className="bio-row">
-                                        <p><span>Email </span>: jsmith@flatlab.com</p>
-                                    </div>
-                                    <div className="bio-row">
-                                        <p><span>Mobile </span>: (12) 03 4567890</p>
-                                    </div>
-                                    <div className="bio-row">
-                                        <p><span>Phone </span>: 88 (02) 123456</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="panel">
-                                        <div className="panel-body">
-                                            <div className="bio-chart">
-                                                <div style="display:inline;width:100px;height:100px;"><canvas width="100" height="100px"></canvas><input className="knob" data-width="100" data-height="100" data-displayprevious="true" data-thickness=".2" value="35" data-fgcolor="#e06b7d" data-bgcolor="#e8e8e8" style={{width: 54px; height: 33px; position: absolute; vertical-align: middle; margin-top: 33px; margin-left: -77px; border: 0px; font-weight: bold; font-style: normal; font-variant: normal; font-stretch: normal; font-size: 20px; line-height: normal; font-family: Arial; text-align: center; color: rgb(224, 107, 125); padding: 0px; -webkit-appearance: none; background: none}};" /></div>
-                                            </div>
-                                            <div className="bio-desk">
-                                                <h4 className="red">Envato Website</h4>
-                                                <p>Started : 15 July</p>
-                                                <p>Deadline : 15 August</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="panel">
-                                        <div className="panel-body">
-                                            <div className="bio-chart">
-                                                <div style="display:inline;width:100px;height:100px;"><canvas width="100" height="100px"></canvas>
-                                                    <input className="knob" data-width="100" data-height="100" data-displayprevious="true" data-thickness=".2" value="63" data-fgcolor="#4CC5CD" data-bgcolor="#e8e8e8"
-                                                        style="width: 54px; height: 33px; position: absolute; vertical-align: middle; margin-top: 33px; margin-left: -77px; border: 0px; font-weight: bold; font-style: normal; font-variant: normal; font-stretch: normal; font-size: 20px; line-height: normal; font-family: Arial; text-align: center; color: rgb(76, 197, 205); padding: 0px; -webkit-appearance: none; background: none;" />
-
-                                                </div>
-                                            </div>
-                                            <div className="bio-desk">
-                                                <h4 className="terques">ThemeForest CMS </h4>
-                                                <p>Started : 15 July</p>
-                                                <p>Deadline : 15 August</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="panel">
-                                        <div className="panel-body">
-                                            <div className="bio-chart">
-                                                <div style="display:inline;width:100px;height:100px;"><canvas width="100" height="100px"></canvas><input className="knob" data-width="100" data-height="100" data-displayprevious="true" data-thickness=".2" value="75" data-fgcolor="#96be4b" data-bgcolor="#e8e8e8"
-                                                    style="{{width: 54px; height: 33px; position: absolute; vertical-align: middle; margin-top: 33px; margin-left: -77px; border: 0px; font-weight: bold; font-style: normal; font-variant: normal; font-stretch: normal; font-size: 20px; line-height: normal; font-family: Arial; text-align: center; color: rgb(150, 190, 75); padding: 0px; -webkit-appearance: none; background: none}}" />
-
-                                                </div>
-                                            </div>
-                                            <div className="bio-desk">
-                                                <h4 className="green">VectorLab Portfolio</h4>
-                                                <p>Started : 15 July</p>
-                                                <p>Deadline : 15 August</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="panel">
-                                        <div className="panel-body">
-                                            <div className="bio-chart">
-                                                <div style="display:inline;width:100px;height:100px;"><canvas width="100" height="100px"></canvas>
-                                                    <input className="knob" data-width="100" data-height="100" data-displayprevious="true" data-thickness=".2" value="50" data-fgcolor="#cba4db" data-bgcolor="#e8e8e8"
-                                                        style="width: 54px; height: 33px; position: absolute; vertical-align: middle; margin-top: 33px; margin-left: -77px; border: 0px; font-weight: bold; font-style: normal; font-variant: normal; font-stretch: normal; font-size: 20px; line-height: normal; font-family: Arial; text-align: center; color: rgb(203, 164, 219); padding: 0px; -webkit-appearance: none; background: none;" />
-
-                                                </div>
-                                            </div>
-                                            <div className="bio-desk">
-                                                <h4 className="purple">Adobe Muse Template</h4>
-                                                <p>Started : 15 July</p>
-                                                <p>Deadline : 15 August</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+           <div
+        className="container-xl px-4 mt-4"
+        style={{ marginLeft: "268px", textAlign: "start" }}
+      >
+        <div className="row">
+          <div className="col-xl-4" style={{ marginTop: "90px" }}>
+            <div className="card mb-4 mb-xl-0"> 
+              <div
+                className="card-header"
+                style={{ backgroundColor: " #263159", color: "white" }}
+              >Profile Picture
+                {/* {item.ProfileImage} */}
+              </div>
+              <div className="card-body text-center">
+                <img
+                  className="img-account-profile rounded-circle mb-2"
+                  src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+                  alt=""
+                />
+              </div>
             </div>
+          </div>
+          <div className="col-xl-8" style={{ textAlign: "start" }}>
+            <div className="card mb-4">
+              <div
+                className="card-header "
+                style={{ backgroundColor: " #263159", color: "white" }}
+              >
+                student Details
+              </div>
+              <div className="card-body">
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">student FName :</label>
+                  {/* <p> {item.fName}</p> */}
+                </div>
+
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">student Lname :</label>
+                  {/* <p> {item.lName}</p> */}
+                </div>
+
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Gender :</label>
+                  {/* <p>{item.gender}</p> */}
+                </div>
+
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Date Of Birth : </label>
+                  {/* <p> {item.dateOfBirth}</p> */}
+                </div>
+
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Contact No :</label>
+                  {/* <p> {item.contactNo}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Date Of Joining :</label>
+                  {/* <p> {item.dateOfJoining}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Qualification :</label>
+                  {/* <p> {item.qualification}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Experiance :</label>
+                  {/* <p> {item.experiance}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Course :</label>
+                  {/* <p> {item.course}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Username :</label>
+                  {/* <p> {item.userName}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Email :</label>
+                  {/* <p> {item.email}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Password :</label>
+                  {/* <p> {item.password}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Repeat Password :</label>
+                  {/* <p> {item.rePassword}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Address :</label>
+                  {/* <p> {item.address}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">City :</label>
+                  {/* <p> {item.city}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">State :</label>
+                  {/* <p> {item.state}</p> */}
+                </div>
+                <div className="col-md-6 d-flex">
+                  <label className="small mb-1">Country :</label>
+                  {/* <p> {item.country}</p> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
         </>
     )
 }
